d0e36c2a84cc44fe43fb1ed7ceaea6aebfa56428
chisgit <chiragpatel@hotmail.com>
2025-05-31 20:35:05 -0400

    Fix double X close button in menu details modal

--- server/index.ts
+++ server/index.ts
@@ -54,17 +54,11 @@ app.use((req, res, next) => {
     await setupVite(app, server);
   } else {
     serveStatic(app);
-  }
-
-  // ALWAYS serve the app on port 5000
+  }  // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+  server.listen(port, () => {
     log(`serving on port ${port}`);
   });
 })();
